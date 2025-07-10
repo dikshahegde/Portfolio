f6993c213a336ed8d1139f57e114a778569cf350
dikshahegde <hegdediksha24@gmail.com>
2025-07-10 08:28:49 +0530

    Build into docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // 🔁 Replace with your GitHub repo name
+  base: '/Portfolio/', 
+  build: {
+    outDir: 'docs',
+  },
 });
